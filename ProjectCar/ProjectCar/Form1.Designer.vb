b5fd68c222dc04e0a627b4b4f164fd54eabdd420
Nauman Shakir <naumanshakir3s@outlook.com>
2015-12-20 07:06:00 +0500

    https://github.com/Nauman3S/ProjectCarRepo.git

---
+++ ProjectCar/ProjectCar/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.up = New System.Windows.Forms.Label()
+        Me.down = New System.Windows.Forms.Label()
+        Me.left = New System.Windows.Forms.Label()
+        Me.right = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'up
+        '
+        Me.up.AutoSize = True
+        Me.up.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.up.Location = New System.Drawing.Point(218, 57)
+        Me.up.Name = "up"
+        Me.up.Size = New System.Drawing.Size(26, 17)
+        Me.up.TabIndex = 0
+        Me.up.Text = "Up"
+        '
+        'down
+        '
+        Me.down.AutoSize = True
+        Me.down.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.down.Location = New System.Drawing.Point(218, 159)
+        Me.down.Name = "down"
+        Me.down.Size = New System.Drawing.Size(46, 17)
+        Me.down.TabIndex = 1
+        Me.down.Text = "Down"
+        '
+        'left
+        '
+        Me.left.AutoSize = True
+        Me.left.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.left.Location = New System.Drawing.Point(127, 103)
+        Me.left.Name = "left"
+        Me.left.Size = New System.Drawing.Size(32, 17)
+        Me.left.TabIndex = 2
+        Me.left.Text = "Left"
+        '
+        'right
+        '
+        Me.right.AutoSize = True
+        Me.right.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.right.Location = New System.Drawing.Point(322, 103)
+        Me.right.Name = "right"
+        Me.right.Size = New System.Drawing.Size(42, 17)
+        Me.right.TabIndex = 3
+        Me.right.Text = "Right"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 232)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Open Port"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(472, 330)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "SendTemp"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 276)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Open Port"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(447, 304)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(572, 498)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.right)
+        Me.Controls.Add(Me.left)
+        Me.Controls.Add(Me.down)
+        Me.Controls.Add(Me.up)
+        Me.Name = "Form1"
+        Me.Text = "ProjectCar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents up As Label
+    Friend WithEvents down As Label
+    Friend WithEvents left As Label
+    Friend WithEvents right As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ ProjectCar/ProjectCar/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Speech.Recognition
+Imports System.Threading
+Imports System.Globalization
+
+Imports System.IO.Ports
+
+
+Public Class Form1
+    ' recogniser & grammar
+    Dim recog As New SpeechRecognizer
+    Dim gram As Grammar
+    ' events
+    Public Event SpeechRecognized As _
+        EventHandler(Of SpeechRecognizedEventArgs)
+    Public Event SpeechRecognitionRejected As _
+        EventHandler(Of SpeechRecognitionRejectedEventArgs)
+    ' word list
+    Dim wordlist As String() = New String() {"Left", "Right", "Up", "Down"}
+
+
+    Public Sub recevent(ByVal sender As System.Object,
+                ByVal e As RecognitionEventArgs)
+        left.ForeColor = Color.LightGray
+        right.ForeColor = Color.LightGray
+        up.ForeColor = Color.LightGray
+        down.ForeColor = Color.LightGray
+
+        If (e.Result.Text = "Left") Then
+            left.ForeColor = Color.Blue
+            SendTo(3)
+        ElseIf (e.Result.Text = "Right") Then
+            right.ForeColor = Color.Blue
+            SendTo(4)
+        ElseIf (e.Result.Text = "Up") Then
+            up.ForeColor = Color.Blue
+            SendTo(1)
+        ElseIf (e.Result.Text = "Down") Then
+            down.ForeColor = Color.Blue
+            SendTo(2)
+        End If
+    End Sub
+    Public Sub recfailevent(ByVal sender As System.Object,
+               ByVal e As RecognitionEventArgs)
+        up.ForeColor = Color.LightGray
+        down.ForeColor = Color.LightGray
+        left.ForeColor = Color.LightGray
+        right.ForeColor = Color.LightGray
+    End Sub
+    ' form initialisation
+    Private Sub Form1_Load(ByVal sender As System.Object,
+            ByVal e As System.EventArgs) Handles MyBase.Load
+        ' need these to get British English rather than default US
+        Thread.CurrentThread.CurrentCulture = New CultureInfo("en-GB")
+        Thread.CurrentThread.CurrentUICulture = New CultureInfo("en-GB")
+        ' convert the word list into a grammar
+        Dim words As New Choices(wordlist)
+        gram = New Grammar(New GrammarBuilder(words))
+        recog.LoadGrammar(gram)
+
+        ' add handlers for the recognition events
+        AddHandler recog.SpeechRecognized, AddressOf Me.recevent
+        AddHandler recog.SpeechRecognitionRejected, AddressOf Me.recfailevent
+        ' enable the recogniser
+        recog.Enabled = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.Close()
+        SerialPort1.PortName = "COM5"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.DataBits = 8
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Open()
+        SerialPort1.Write(TextBox1.Text)
+        SerialPort1.Close()
+    End Sub
+    Sub SendTo(ByVal val As Integer)
+        SerialPort1.Open()
+        SerialPort1.Write(val)
+        SerialPort1.Close()
+
+    End Sub
+
+
+End Class
