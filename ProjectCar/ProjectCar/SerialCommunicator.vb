d5d3971454050b18ebc888785024916978a4cc5d
Nauman Shakir <naumanshakir3s@outlook.com>
2015-12-31 07:14:01 +0500

    new module

---
+++ ProjectCar/ProjectCar/SerialCommunicator.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO.Ports
+Module SerialCommunicator
+
+    Sub OpenPortC()
+        Form1.SerialPort1.Close()
+        Form1.SerialPort1.PortName = "COM5"
+        Form1.SerialPort1.BaudRate = 9600
+        Form1.SerialPort1.DataBits = 8
+        Form1.SerialPort1.Parity = Parity.None
+        Form1.SerialPort1.StopBits = StopBits.One
+        Form1.SerialPort1.Handshake = Handshake.None
+        Form1.SerialPort1.Encoding = System.Text.Encoding.Default
+    End Sub
+    Sub SendValue(ByVal val As Integer)
+        Form1.SerialPort1.Open()
+        Form1.SerialPort1.Write(val)
+        Form1.SerialPort1.Close()
+
+    End Sub
+End Module
